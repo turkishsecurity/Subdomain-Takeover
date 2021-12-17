206b19e2f4148186ef4e137ffb92ac663524a5b3
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-12-17 22:41:29 +0300

    Update takeover.py

--- takeover.py
+++ takeover.py
@@ -1,49 +1,36 @@
+import urllib.request
+import os 
 import requests
-import os
 import sys
-from colorama import Fore, Back, Style
-import colorama
 import time
+import colorama
+from colorama import Back, Fore, Style
 
-banner = """
-[!] WELCOME TO SUBDOMAIN TAKEOVER TESTER TOOL [!]
-
-Coded by Xale
-GitHub: @xaletr
-
-"""
-print(Fore.GREEN + banner)
-
-if sys.argv[1] in ['-u', '--url']:
- url = sys.argv[2]
- resp = requests.get(url)
- #payloads
- spayload = '<div id="shop-not-found">'
- hpayload = '<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>'
- apayload = '<li>Message: The specified bucket does not exist</li>'
-#shopify
-
- print(Fore.BLUE)
- print("Testing : Shopify Subdomain Takeover")
+if sys.argv[1] in ['-t', '--targets', '-T']:
+ filename = sys.argv[2]
  time.sleep(3)
-if spayload in resp.text:
- print(Fore.GREEN + "[FOUND]" + Fore.BLUE + " Vulnerable Found!")
-else:
- print(Fore.RED + "[NOT FOUND]" + Fore.BLUE + " Vulnerable Found!")
+ print("-----------------------------")
+ print("SUBDOMAIN TAKEOVER TESTER ")
+ print("CODED BY XALE ~ @xaletr")
+ print("-----------------------------")
+file = open(filename, "r")
+lines = file.readlines()
 
-#heroku
-print("Testing : Heroku Subdomain Takeover")
-time.sleep(3)
-if hpayload in resp.text:
- print(Fore.GREEN + "[FOUND]" + Fore.BLUE + " Vulnerable Found!")
-else:
- print(Fore.RED + "[NOT FOUND]" + Fore.BLUE + " Vulnerable Found!")
 
-#aws
-print("Testing : Amazon AWS Subdomain Takeover")
-time.sleep(3)
-if apayload in resp.text:
- print(Fore.GREEN + "[FOUND]" + Fore.BLUE + " Vulnerable Found!")
-else:
- print(Fore.RED + "[NOT FOUND]" + Fore.BLUE + " Vulnerable Found!")
- 
+for site in lines:
+  try:
+   req = requests.get(site)
+    #payloads
+   spayload = '<div id="shop-not-found">'
+   hpayload = '<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>'
+   apayload = '<li>Message: The specified bucket does not exist</li>'
+   if spayload in req.text:
+    print(Fore.GREEN + "[!] BASARILI : "+req.url+" [SHOPIFY]")
+   elif hpayload in req.text:
+    print(Fore.GREEN + "[!] BASARILI : "+req.url+Fore.PURPLE+" [HEROKU]")
+   elif apayload in req.text:
+    print(Fore.GREEN + "[!] BASARILI : "+req.url+Fore.RED+" [AWS]")
+   else:
+    print("[!] HATA "+site)
+  except:
+   print("[!] HATA "+site)
